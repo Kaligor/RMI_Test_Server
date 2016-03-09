b0658c2da68ed2e301740926fef8e868d380b393
Kaligor <Kaligor5@hotmail.com>
2016-03-09 16:25:56 +0100

    Card Logic have implementations for all Interface Methods

--- src/com/dragonfire/GameLogic.java
+++ src/com/dragonfire/GameLogic.java
@@ -17,8 +17,8 @@ public class GameLogic extends UnicastRemoteObject implements GameLogicInterface
 
     //<editor-fold defaultstate="collapsed" desc="Deck Values">
     ArrayList<Card> deck = new ArrayList<>();
-    long seed = System.nanoTime();
-    Card joker = new Card("Joker", 0);
+    long seed; 
+    Card joker = new Card("Joker", 0, "black_joker.png");
 //</editor-fold>
 
     int deckSize;
@@ -127,80 +127,110 @@ public class GameLogic extends UnicastRemoteObject implements GameLogicInterface
     @Override
     public final Card drawCardFromDeck(int Player) throws RemoteException
     {
-        if (deck.size() > 0)
+        if (deck.size() > 0) 
         {
             shuffleDeck();
-            Card card = new Card(deck.get(0).face, deck.get(0).value);
+            Card card = new Card(deck.get(0).face, deck.get(0).value, deck.get(0).imageName);
             deck.remove(0);
             return card;
         }
-
         return joker;
+    }
+    
+    public int getDeckSizeByPlayer(int Player) {
+        switch (Player)
+        {
+            case 1:
+                return player1Hand;
+            case 2:
+                return player2Hand;
+            default:
+                return -1;
+        }
     }
 
     @Override
     public final void shuffleDeck() throws RemoteException
     {
+        seed = System.nanoTime();
         Collections.shuffle(deck, new Random(seed));
     }
 
+    @Override
+    public void disconnect(int Player) throws RemoteException
+    {
+        nextPlayer--;
+        if(Player == 1) {
+            Player1 = false;
+        } else if (Player == 2) {
+            Player2 = false;
+        }
+    }
+
+    @Override
+    public void reset() throws RemoteException
+    {
+        deck.clear();
+        fillDeck();
+    }
+    
     public final void fillDeck()
     {
-        deck.add(new Card(Card.HEART, 1));
-        deck.add(new Card(Card.HEART, 2));
-        deck.add(new Card(Card.HEART, 3));
-        deck.add(new Card(Card.HEART, 4));
-        deck.add(new Card(Card.HEART, 5));
-        deck.add(new Card(Card.HEART, 6));
-        deck.add(new Card(Card.HEART, 7));
-        deck.add(new Card(Card.HEART, 8));
-        deck.add(new Card(Card.HEART, 9));
-        deck.add(new Card(Card.HEART, 10));
-        deck.add(new Card(Card.HEART, 11));
-        deck.add(new Card(Card.HEART, 12));
-        deck.add(new Card(Card.HEART, 13));
+        deck.add(new Card(Card.HEART, 1, "ace_of_hearts.png"));
+        deck.add(new Card(Card.HEART, 2, "2_of_hearts.png"));
+        deck.add(new Card(Card.HEART, 3, "3_of_hearts.png"));
+        deck.add(new Card(Card.HEART, 4, "4_of_hearts.png"));
+        deck.add(new Card(Card.HEART, 5, "5_of_hearts.png"));
+        deck.add(new Card(Card.HEART, 6, "6_of_hearts.png"));
+        deck.add(new Card(Card.HEART, 7, "7_of_hearts.png"));
+        deck.add(new Card(Card.HEART, 8, "8_of_hearts.png"));
+        deck.add(new Card(Card.HEART, 9, "9_of_hearts.png"));
+        deck.add(new Card(Card.HEART, 10, "10_of_hearts.png"));
+        deck.add(new Card(Card.HEART, 11, "jack_of_hearts2.png"));
+        deck.add(new Card(Card.HEART, 12, "queen_of_hearts2.png"));
+        deck.add(new Card(Card.HEART, 13, "king_of_hearts2.png"));
 
-        deck.add(new Card(Card.DIAMOND, 1));
-        deck.add(new Card(Card.DIAMOND, 2));
-        deck.add(new Card(Card.DIAMOND, 3));
-        deck.add(new Card(Card.DIAMOND, 4));
-        deck.add(new Card(Card.DIAMOND, 5));
-        deck.add(new Card(Card.DIAMOND, 6));
-        deck.add(new Card(Card.DIAMOND, 7));
-        deck.add(new Card(Card.DIAMOND, 8));
-        deck.add(new Card(Card.DIAMOND, 9));
-        deck.add(new Card(Card.DIAMOND, 10));
-        deck.add(new Card(Card.DIAMOND, 11));
-        deck.add(new Card(Card.DIAMOND, 12));
-        deck.add(new Card(Card.DIAMOND, 13));
+        deck.add(new Card(Card.DIAMOND, 1, "ace_of_diamonds.png"));
+        deck.add(new Card(Card.DIAMOND, 2, "2_of_diamonds.png"));
+        deck.add(new Card(Card.DIAMOND, 3, "3_of_diamonds.png"));
+        deck.add(new Card(Card.DIAMOND, 4, "4_of_diamonds.png"));
+        deck.add(new Card(Card.DIAMOND, 5, "5_of_diamonds.png"));
+        deck.add(new Card(Card.DIAMOND, 6, "6_of_diamonds.png"));
+        deck.add(new Card(Card.DIAMOND, 7, "7_of_diamonds.png"));
+        deck.add(new Card(Card.DIAMOND, 8, "8_of_diamonds.png"));
+        deck.add(new Card(Card.DIAMOND, 9, "9_of_diamonds.png"));
+        deck.add(new Card(Card.DIAMOND, 10, "10_of_diamonds.png"));
+        deck.add(new Card(Card.DIAMOND, 11, "jack_of_diamonds2.png"));
+        deck.add(new Card(Card.DIAMOND, 12, "queen_of_diamonds2.png"));
+        deck.add(new Card(Card.DIAMOND, 13, "king_of_diamonds2.png"));
 
-        deck.add(new Card(Card.CLUB, 1));
-        deck.add(new Card(Card.CLUB, 2));
-        deck.add(new Card(Card.CLUB, 3));
-        deck.add(new Card(Card.CLUB, 4));
-        deck.add(new Card(Card.CLUB, 5));
-        deck.add(new Card(Card.CLUB, 6));
-        deck.add(new Card(Card.CLUB, 7));
-        deck.add(new Card(Card.CLUB, 8));
-        deck.add(new Card(Card.CLUB, 9));
-        deck.add(new Card(Card.CLUB, 10));
-        deck.add(new Card(Card.CLUB, 11));
-        deck.add(new Card(Card.CLUB, 12));
-        deck.add(new Card(Card.CLUB, 13));
+        deck.add(new Card(Card.CLUB, 1, "ace_of_clubs.png"));
+        deck.add(new Card(Card.CLUB, 2, "2_of_clubs.png"));
+        deck.add(new Card(Card.CLUB, 3, "3_of_clubs.png"));
+        deck.add(new Card(Card.CLUB, 4, "4_of_clubs.png"));
+        deck.add(new Card(Card.CLUB, 5, "5_of_clubs.png"));
+        deck.add(new Card(Card.CLUB, 6, "6_of_clubs.png"));
+        deck.add(new Card(Card.CLUB, 7, "7_of_clubs.png"));
+        deck.add(new Card(Card.CLUB, 8, "8_of_clubs.png"));
+        deck.add(new Card(Card.CLUB, 9, "9_of_clubs.png"));
+        deck.add(new Card(Card.CLUB, 10, "10_of_clubs.png"));
+        deck.add(new Card(Card.CLUB, 11, "jack_of_clubs2.png"));
+        deck.add(new Card(Card.CLUB, 12, "queen_of_clubs2.png"));
+        deck.add(new Card(Card.CLUB, 13, "king_of_clubs2.png"));
 
-        deck.add(new Card(Card.SPADE, 1));
-        deck.add(new Card(Card.SPADE, 2));
-        deck.add(new Card(Card.SPADE, 3));
-        deck.add(new Card(Card.SPADE, 4));
-        deck.add(new Card(Card.SPADE, 5));
-        deck.add(new Card(Card.SPADE, 6));
-        deck.add(new Card(Card.SPADE, 7));
-        deck.add(new Card(Card.SPADE, 8));
-        deck.add(new Card(Card.SPADE, 9));
-        deck.add(new Card(Card.SPADE, 10));
-        deck.add(new Card(Card.SPADE, 11));
-        deck.add(new Card(Card.SPADE, 12));
-        deck.add(new Card(Card.SPADE, 13));
+        deck.add(new Card(Card.SPADE, 1, "ace_of_spades.png"));
+        deck.add(new Card(Card.SPADE, 2, "2_of_spades.png"));
+        deck.add(new Card(Card.SPADE, 3, "3_of_spades.png"));
+        deck.add(new Card(Card.SPADE, 4, "4_of_spades.png"));
+        deck.add(new Card(Card.SPADE, 5, "5_of_spades.png"));
+        deck.add(new Card(Card.SPADE, 6, "6_of_spades.png"));
+        deck.add(new Card(Card.SPADE, 7, "7_of_spades.png"));
+        deck.add(new Card(Card.SPADE, 8, "8_of_spades.png"));
+        deck.add(new Card(Card.SPADE, 9, "9_of_spades.png"));
+        deck.add(new Card(Card.SPADE, 10, "10_of_spades.png"));
+        deck.add(new Card(Card.SPADE, 11, "jack_of_spades2.png"));
+        deck.add(new Card(Card.SPADE, 12, "queen_of_spades2.png"));
+        deck.add(new Card(Card.SPADE, 13, "king_of_spades2.png"));
     }
 
 }

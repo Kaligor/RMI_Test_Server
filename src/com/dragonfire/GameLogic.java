8ebf097b7609864a095f276db8d949f4650df856
Kaligor <Kaligor5@hotmail.com>
2016-03-09 10:43:54 +0100

    fillDeck method (fills <Deck> with a standard deck of 52 Cards) drawCardFromDeck: Implemented but untested GetPlayerHandSize: Unsupported Also cleaned the code and added Descripion Blocks

--- src/com/dragonfire/GameLogic.java
+++ src/com/dragonfire/GameLogic.java
@@ -2,23 +2,44 @@ package com.dragonfire;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class GameLogic extends UnicastRemoteObject implements GameLogicInterface
 {
 
+    //<editor-fold defaultstate="collapsed" desc="Old Game Values">
     int value1 = 0;
     int value2 = 0;
     int mainValue = 0;
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Deck Values">
+    ArrayList<Card> deck = new ArrayList<>();
+    long seed = System.nanoTime();
+    Card joker = new Card("Joker", 0);
+//</editor-fold>
+
+    int deckSize;
+    int player1Hand;
+    int player2Hand;
+    
+    //<editor-fold defaultstate="collapsed" desc="2 Player Multiplayer Functonallity Values">
     boolean Player1 = false;
     boolean Player2 = false;
     static int nextPlayer = 0;
+    //</editor-fold>
 
     public GameLogic() throws RemoteException
     {
         super();
+        fillDeck();
     }
 
+    //<editor-fold defaultstate="collapsed" desc="Old Game Logics">
     @Override
+    @Deprecated
     public void increaseValue(int i) throws RemoteException
     {
         if (i == 1)
@@ -33,12 +54,13 @@ public class GameLogic extends UnicastRemoteObject implements GameLogicInterface
     }
 
     @Override
+    @Deprecated
     public int getValue(int i) throws RemoteException
     {
         switch (i)
         {
             case 1:
-                return value1;       
+                return value1;
             case 2:
                 return value2;
             case 9:
@@ -47,7 +69,9 @@ public class GameLogic extends UnicastRemoteObject implements GameLogicInterface
                 return -1;
         }
     }
+//</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="2 Player Multiplayer Functionallity">
     @Override
     public int assignPlayer() throws RemoteException
     {
@@ -84,6 +108,88 @@ public class GameLogic extends UnicastRemoteObject implements GameLogicInterface
     public boolean getReady() throws RemoteException
     {
         return Player1 == true && Player2 == true;
+    }
+//</editor-fold>
+
+    @Override
+    public int getPlayerHandSize() throws RemoteException
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Card drawCardFromDeck(int Player) throws RemoteException
+    {
+        if(deck.size() > 0) {
+            return deck.get(0);
+        }
+        
+        return joker;
+    }
+
+    @Override
+    public void shuffleDeck() throws RemoteException
+    {
+        Collections.shuffle(deck, new Random(seed));
+    }
+
+    public final void fillDeck()
+    {
+        deck.add(new Card(Card.HEART, 1));
+        deck.add(new Card(Card.HEART, 2));
+        deck.add(new Card(Card.HEART, 3));
+        deck.add(new Card(Card.HEART, 4));
+        deck.add(new Card(Card.HEART, 5));
+        deck.add(new Card(Card.HEART, 6));
+        deck.add(new Card(Card.HEART, 7));
+        deck.add(new Card(Card.HEART, 8));
+        deck.add(new Card(Card.HEART, 9));
+        deck.add(new Card(Card.HEART, 10));
+        deck.add(new Card(Card.HEART, 11));
+        deck.add(new Card(Card.HEART, 12));
+        deck.add(new Card(Card.HEART, 13));
+
+        deck.add(new Card(Card.DIAMOND, 1));
+        deck.add(new Card(Card.DIAMOND, 2));
+        deck.add(new Card(Card.DIAMOND, 3));
+        deck.add(new Card(Card.DIAMOND, 4));
+        deck.add(new Card(Card.DIAMOND, 5));
+        deck.add(new Card(Card.DIAMOND, 6));
+        deck.add(new Card(Card.DIAMOND, 7));
+        deck.add(new Card(Card.DIAMOND, 8));
+        deck.add(new Card(Card.DIAMOND, 9));
+        deck.add(new Card(Card.DIAMOND, 10));
+        deck.add(new Card(Card.DIAMOND, 11));
+        deck.add(new Card(Card.DIAMOND, 12));
+        deck.add(new Card(Card.DIAMOND, 13));
+
+        deck.add(new Card(Card.CLUB, 1));
+        deck.add(new Card(Card.CLUB, 2));
+        deck.add(new Card(Card.CLUB, 3));
+        deck.add(new Card(Card.CLUB, 4));
+        deck.add(new Card(Card.CLUB, 5));
+        deck.add(new Card(Card.CLUB, 6));
+        deck.add(new Card(Card.CLUB, 7));
+        deck.add(new Card(Card.CLUB, 8));
+        deck.add(new Card(Card.CLUB, 9));
+        deck.add(new Card(Card.CLUB, 10));
+        deck.add(new Card(Card.CLUB, 11));
+        deck.add(new Card(Card.CLUB, 12));
+        deck.add(new Card(Card.CLUB, 13));
+
+        deck.add(new Card(Card.SPADE, 1));
+        deck.add(new Card(Card.SPADE, 2));
+        deck.add(new Card(Card.SPADE, 3));
+        deck.add(new Card(Card.SPADE, 4));
+        deck.add(new Card(Card.SPADE, 5));
+        deck.add(new Card(Card.SPADE, 6));
+        deck.add(new Card(Card.SPADE, 7));
+        deck.add(new Card(Card.SPADE, 8));
+        deck.add(new Card(Card.SPADE, 9));
+        deck.add(new Card(Card.SPADE, 10));
+        deck.add(new Card(Card.SPADE, 11));
+        deck.add(new Card(Card.SPADE, 12));
+        deck.add(new Card(Card.SPADE, 13));
     }
 
 }
